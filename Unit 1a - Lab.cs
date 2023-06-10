83f8d35df5e365d72dfab57cd83a1231a2bb6d15
Morgan VanWagoner <10784675@uvu.edu>
2023-06-10 17:19:02 -0600

    Practicing Code

---
+++ Unit 1a - Lab.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+public class Program
+{
+    public Weapon miniGun;
+
+    public void Main()
+    {
+        miniGun = new Weapon();
+        Console.WriteLine(miniGun.weaponName);
+    }
+}
+
+public class Weapon
+{
+    public int ammoCount = 10;
+    public string weaponName = "Mini Gun";
+    public float firePower = 11.5f;
+}
