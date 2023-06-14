886053156a352205c4955f323092378c4c2296ec
Morgan VanWagoner <10784675@uvu.edu>
2023-06-13 19:55:28 -0600

    Unit 1b - Lab
    Void Functions

---
+++ Unit 1b - Lab.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public void Main()
+    {
+        Console.WriteLine("Hello World");
+        DoMath(10, 4);
+        DoMath(20, 7);
+        DoMath(30, 15);
+    }
+
+    public void DoMath(int value, int value2)
+    {
+        var number = value + 2;
+        Console.WriteLine(number);
+    }
+}
